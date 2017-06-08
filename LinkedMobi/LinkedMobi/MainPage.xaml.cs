7eae3504f981ff4eb8d8723471fdbe30231c3e41
Elvis Khosa <Elvis Khosa>
2017-06-08 15:57:49 +0200

    Add project files.

---
+++ LinkedMobi/LinkedMobi/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LinkedMobi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
